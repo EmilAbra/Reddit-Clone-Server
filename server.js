af3e0bf9094442b322b123d0e628cfc94d1e19c3
eabra <emab21@student.bth.se>
2024-03-07 13:57:11 +0100

    Add Comments Route

--- server.js
+++ server.js
@@ -2,17 +2,30 @@ import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import sensible from "@fastify/sensible";
 import cors from "@fastify/cors";
+import cookie from "@fastify/cookie";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = fastify();
 app.register(sensible);
+app.register(cookie, { secret: process.env.COOKIE_SECRET });
 app.register(cors, {
   origin: process.env.CLIENT_URL,
   credentials: true,
 });
 
+app.addHook("onRequest", (req, res, done) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID;
+    res.clearCookie("userId");
+    res.setCookie("userId", CURRENT_USER_ID);
+  }
+  done();
+});
+
 const prisma = new PrismaClient();
+const CURRENT_USER_ID = await prisma.user.findFirst({ where: { name: "Kyle" } })
+  .id;
 
 app.get("/posts", async (req, res) => {
   return await commitToDb(
@@ -49,6 +62,23 @@ app.get("/posts/:id", async (req, res) => {
             },
           },
         },
+      },
+    })
+  );
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  if (req.body.message === "" || req.body.message == null) {
+    return res.send(app.httpErrors.badRequest("Message is required"));
+  }
+
+  return await commitToDb(
+    prisma.comment.create({
+      data: {
+        message: req.body.message,
+        userId: req.cookies.userId,
+        parentId: req.body.parentId,
+        postId: req.params.id,
       },
     })
   );
